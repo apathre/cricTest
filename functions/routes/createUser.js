6facc4f688949c8f6eb4c38e7d35c293c6a056d3
Aditya Pathre <adi.iitk@gmail.com>
2019-09-24 22:18:56 +0100

    v2

--- functions/routes/createUser.js
+++ functions/routes/createUser.js
@@ -6,21 +6,22 @@ let db=admin.firestore();
 exports.post=(req,res)=>{
     console.log('body:',db.collection('users'));
     let id=req.body['messenger user id'];
-    let docRef = db.collection('users').doc('id');
+    let data={
+        "messenger_id":req.body['messenger user id'],
+        "first_name":req.body['first name'],
+        "last_name":req.body['last name'],
+        "gender":req.body['last name'],
+        "signed_up":req.body['signed up'],
+        "last_seen":req.body['last seen'],
+        "game_number":0,
+        "coins":0,
+        "userWins":0,
+        "compWins":0,
+        "winRatio":0
+        }
+    let docRef = db.collection('users').doc(data.messenger_id);
 
-    let setAda = docRef.set({
-    "messenger_id":req.body['messenger user id'],
-    "first_name":req.body['first name'],
-    "last_name":req.body['last name'],
-    "gender":req.body['last name'],
-    "signed_up":req.body['signed up'],
-    "last_seen":req.body['last seen'],
-    "game_number":'0',
-    "coins":'0',
-    "userWins":'0',
-    "compWins":'0',
-    "winRatio":'0'
-    });
+    let setAda = docRef.set(data);
   
     res.status(200);
 
