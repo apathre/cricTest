5b2af26fdfc99b90969acb558ea01e6b91665e83
Aditya Pathre <adi.iitk@gmail.com>
2019-09-28 01:38:52 +0100

    mnjh

--- functions/routes/updateWin.js
+++ functions/routes/updateWin.js
@@ -5,10 +5,22 @@ let db=admin.firestore();
 
 exports.post=(req,res)=>{
     var bet=req.body.bet;
-    var tResult='';
-    var multiple=1.8;
+    var pitch_type=req.body.pitch_type;
+    
     let data={
         messenger_id:req.body['messenger user id']
+    }
+
+    var tResult='';
+    var multiple=0;
+    if(pitch_type==='dead'){
+        multiple=1.4;
+    }
+    else if(pitch==='green'){
+        multiple=1.6;
+    }
+    else{
+        multiple=1.8;
     }
 
     let userRef = db.collection('users').doc(`${data.messenger_id}`);
@@ -21,6 +33,16 @@ exports.post=(req,res)=>{
     }).then(result => {
     console.log('Transaction success', result);
     tResult='success';
+    res.send({
+        "set_attributes":{
+            "updatedWin":tResult
+        },
+        "messages":[
+            {
+                "text":`Coin Balance increased 💰💰 \nCurrent Coins: ${newCoins}`
+            }
+        ]
+    });
     return 0;
     }).catch(err => {
     console.log('Transaction failure:', err);
@@ -28,9 +50,5 @@ exports.post=(req,res)=>{
     });
  });
 
-    res.send({
-        "set_attributes":{
-            "updatedWin":tResult
-        }
-    });
+    
 }
