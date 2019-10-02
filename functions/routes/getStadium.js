bf04f7f233fe8f6e9e0fcd4aadfeadea5afa9d81
Aditya Pathre <adi.iitk@gmail.com>
2019-10-02 22:23:28 +0100

    nsjsk

--- functions/routes/getStadium.js
+++ functions/routes/getStadium.js
@@ -5,6 +5,7 @@ exports.post=(req,res)=>{
     var rnd=sms.random(5,0);
     var stadiumImage=fns.stadiumImages[rnd];
     var stadiumText=fns.stadiumImages[rnd];
+    console.log('stadium text:',stadiumText,stadiumImage);
 
     res.send(
         {
