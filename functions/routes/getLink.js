d33a967a0b30b15ce76a1108dfb738bc54c56bff
Aditya Pathre <pathreaig@gmail.com>
2019-12-06 01:11:21 +0530

    updated link section

--- functions/routes/getLink.js
+++ functions/routes/getLink.js
@@ -1,28 +1,36 @@
+const nodeMailer = require('nodemailer');
 exports.post=(req,res)=>{
-    const data={
-        id:req.body['messenger user id'],
-        first_name:req.body['first name'],
-        last_name:req.body['last name']
+    
+  const transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,  //true for 465 port, false for other ports
+    auth: {
+      user: 'pathreaig@gmail.com',
+      pass: 'bh1w1ni!'
     }
-    let url=`https://www.swadi.in/wp/checkout?FirstName=${data.first_name}&LastName=${data.last_name}&id=${data.id}`;
-
+  });
+  const mailOptions = {
+    from: '"Aditya Pathre" <pathreaig@gmail.com>', // sender address
+    to: req.body.email, // list of receivers
+    subject: 'Hello ', // Subject line
+    text: `Hi, Click on this link to get your miniover coins: https://www.swadi.in/wp/checkout?FirstName=${data.first_name}&LastName=${data.last_name}&id=${data.id}`
+    , // plain text body
+    html: '<b>Hello world?</b>' // html body
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(400).send({success: false})
+    } else {
+      res.status(200).send({success: true});
+    }
+  });
+    
     res.send({
         "messages":[
             {
-                "attachment":{
-                    "type":"template",
-                    "payload":{
-                        "template_type":"button",
-                        "text":"Click this button to go for payment link",
-                        "buttons":[
-                            {
-                                "type":"web_url",
-                                "url":url,
-                                "title":"Payment Link"
-                            }
-                        ]
-                    }
-                }
+                "text":"Link has been sent on you email id !!"
             }
         ]
     });
